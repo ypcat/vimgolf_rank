d686cbf219fbf02c27c432eadb4da6797806a4cd
ypcat <ypcat6@gmail.com>
2012-02-05 21:26:46 +0800

    change update data using gae task in the background

--- app.py
+++ app.py
@@ -2,13 +2,15 @@
 
 import logging
 import urllib
+from google.appengine.api import taskqueue
 from google.appengine.ext import db
 from BeautifulSoup import BeautifulSoup
 import web
 
 urls = (
     '/', 'index',
-    '/update', 'update',
+    '/user', 'user',
+    '/top', 'top',
 )
 
 class index:
@@ -18,6 +20,34 @@ class index:
         golfers = top_golfers()
         for golfer in golfers:
             print '<div>%d %s %d</div>' % (golfer.rank, golfer.handle, golfer.g_rank)
+
+class user:
+    def POST(self):
+        """Update user rank and store in database"""
+        i = web.input()
+        golfer = Golfer()
+        golfer.handle = i.handle
+        golfer.rank = int(i.rank)
+        golfer.g_rank = global_rank(golfer.handle)
+        golfer.put()
+
+class top:
+    def POST(self):
+        """Update LeaderBaord and store in database"""
+        html = fetch('top')
+        soup = BeautifulSoup(html)
+        rows = soup.findAll('h6')[1:] # skip first <h6>, not user record
+        handles = [r.a.text[1:] for r in rows]
+
+        leaderboard = LeaderBoard.gql('').get()
+        if not leaderboard:
+            leaderboard = LeaderBoard()
+        leaderboard.handles = handles
+        leaderboard.put()
+
+class LeaderBoard(db.Model):
+    """Top 100 user handles. This should have only one record."""
+    handles = db.StringListProperty()
 
 class Golfer(db.Model):
     """Data model for user"""
@@ -49,25 +79,20 @@ def global_rank(handle):
 def top_golfers():
     """Returns top 100 Golfer."""
 
-    query = Golfer.gql("where rank <= 100")
-    if query.count() == 100:
-        return list(query)
+    leaderboard = LeaderBoard.gql('').get()
+    if not leaderboard:
+        taskqueue.add(url='/top')
+        return []
 
+    handles = leaderboard.handles
     golfers = []
-    html = fetch('top')
-    soup = BeautifulSoup(html)
-    rows = soup.findAll('h6')[1:] # skip first <h6>, not user record
-    for i, r in enumerate(rows):
-        handle = r.a.text[1:] # skip '@' in @handle
+    for i, handle in enumerate(handles):
         golfer = Golfer.gql("where handle = '%s'" % handle).get()
-        logging.info('%d %s %s'%(i+1,handle,repr(golfer)))
-        if not golfer:
-            golfer = Golfer()
-            golfer.handle = handle
-            golfer.rank = i + 1
-            golfer.g_rank = global_rank(golfer.handle)
-            golfer.put()
-        golfers.append(golfer)
+        if golfer:
+            golfers.append(golfer)
+        else:
+            taskqueue.add(url='/user', params={'handle':handle, 'rank':i+1})
+
     return golfers
 
 if __name__ == '__main__':
